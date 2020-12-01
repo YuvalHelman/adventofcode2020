711bd683ef0268338a527abf0730495b122196cd
yhelman <yhelman@cisco.com>
2020-12-01 18:54:13 +0200

    day1

--- test/test_day1.py
+++ test/test_day1.py
@@ -5,9 +5,14 @@ from adventOfCode import day1
 
 @fixture
 def data():
-    return [2, 3, 1, 8, 1, 4]
+    return [2, 3, 1, 8, 1, 4, 3]
 
 
-def test_ex1():
+def test_ex1(data):
     res = day1.ex1(data, 7)
-    assert res == 12
+    assert res == 4 * 3
+
+
+def test_ex2(data):
+    res = day1.ex2(data, 7)
+    assert res == 2 * 4 * 1
